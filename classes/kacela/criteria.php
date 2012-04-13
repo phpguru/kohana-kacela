39e8762d03544925b8e1e74b355bbd5052394f96
Noah Goodrich <goodrich.noah@gmail.com>
2012-04-13 17:32:02 -0600

    Updated kacela\criteria to support or's

--- classes/kacela/criteria.php
+++ classes/kacela/criteria.php
@@ -1,9 +1,9 @@
 <?php
-/** 
+/**
  * @author Noah Goodrich
  * @date 8/6/11
  * @brief
- * 
+ *
 */
 
 namespace Kacela;
@@ -12,38 +12,38 @@ use Gacela as G;
 
 class Criteria extends G\Criteria {
 
-	public function greater_than($field, $value)
+	public function greater_than($field, $value, $or = false)
 	{
 		return $this->greaterThan($field, $value);
 	}
 
-	public function is_null($field)
+	public function is_null($field, $or = false)
 	{
 		return $this->isNull($field);
 	}
 
-	public function is_not_null($field)
+	public function is_not_null($field, $or = false)
 	{
 
 		return $this->isNotNull($field);
 	}
 
-	public function less_than($field, $value)
+	public function less_than($field, $value, $or = false)
 	{
 		return $this->lessThan($field, $value);
 	}
 
-	public function not_equals($field, $value)
+	public function not_equals($field, $value, $or = false)
 	{
 		return $this->notEquals($field, $value);
 	}
 
-	public function not_in($field, array $value)
+	public function not_in($field, array $value, $or = false)
 	{
 		return $this->notIn($field, $value);
 	}
 
-	public function not_like($field, $value)
+	public function not_like($field, $value, $or = false)
 	{
 		return $this->notLike($field, $value);
 	}
