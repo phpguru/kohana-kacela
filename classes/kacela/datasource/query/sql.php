db08cd8154baf063961e78daa4a185c0ddecf49d
Noah Goodrich <goodrich.noah@gmail.com>
2012-08-28 22:08:19 -0600

    Added Sql::left_join and right_join methods

---
+++ classes/kacela/datasource/query/sql.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kacela\DataSource\Query;
+
+use Gacela\DataSource\Query as Q;
+
+class Sql extends Q\Sql {
+
+	/**
+	 * A convenience wrapper for join
+	 * @param  string|array $table
+	 * @param  string $on
+	 * @param array $columns
+	 * @param string $type
+	 * @return Sql
+	 */
+	public function left_join($table, $on, array $columns = array())
+	{
+		return $this->leftJoin($table, $on, $columns);
+	}
+
+	/**
+	 * A convenience wrapper for join
+	 * @param  string|array $table
+	 * @param  string $on
+	 * @param array $columns
+	 * @param string $type
+	 * @return Sql
+	 */
+	public function right_join($table, $on, array $columns = array())
+	{
+		return $this->rightJoin($table, $on, $columns);
+	}
+
+}
