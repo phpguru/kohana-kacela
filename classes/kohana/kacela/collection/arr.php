e325b1b4c02d439433ee6ea4ca8414ccf235287d
Noah Goodrich <goodrich.noah@gmail.com>
2012-10-24 20:32:37 -0600

    Collection::as_array() is working for kacela

--- classes/kohana/kacela/collection/arr.php
+++ classes/kohana/kacela/collection/arr.php
@@ -4,5 +4,8 @@ use Gacela\Collection as C;
 
 class Kohana_Kacela_Collection_Arr extends C\Arr
 {
-
+	public function as_array()
+	{
+		return call_user_func_array('parent::asArray', func_get_args());
+	}
 }
--- classes/kohana/kacela/collection/statement.php
+++ classes/kohana/kacela/collection/statement.php
@@ -4,5 +4,8 @@ use Gacela\Collection as C;
 
 class Kohana_Kacela_Collection_Statement extends C\Statement
 {
-
+	public function as_array()
+	{
+		return call_user_func_array('parent::asArray', func_get_args());
+	}
 }
